cf3be80738989ee78e44702e27123ad1dfbc7a87
Dereje Gudisa <derejegudisaeme@gmail.com>
2026-02-13 20:52:21 +0300

    fetching data from api is done

---
+++ src/recent.jsx
@@ -0,0 +1,18 @@
+
+function Recent(){
+
+    return ( 
+        <div className="side-bar">
+            <h3>Recent Searches</h3>
+            <div className="recent-lists">
+                <p>London</p>
+                <p>New York</p>
+                <p>Tokyo</p>
+                <p>Paris</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Recent
--- src/weather.jsx
+++ src/weather.jsx
@@ -1,33 +1,77 @@
+import { useState } from "react";
+import Recent from "./recent";
 
 function Weather() {
+    async function getWeatherData(city){
+        const apiKey = "813f9e6b231ee48528557a605da1cb88";
+        const apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        
+        const response = await fetch(apiUrl);
+        //console.log(response);
 
-  return(
-    <div className="container">
-        <div className="side-bar">
-            <h3>Recent Searches</h3>
-            <div className="recent-lists">
-                <p>London</p>
-                <p>New York</p>
-                <p>Tokyo</p>
-                <p>Paris</p>
-            </div>
-        </div>
-        <div className="forecast-container">
-            <div className="seach-bar">
-                <input type="text" className="" placeholder="Search city" />
-                <button type="">Search</button>
-            </div>
+        if(!response.ok){
+            throw new Error("it seems data not found");
+        }
+         
+        return await response.json();
+
+    };
+
+    
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState(null);
+
+    
+    async function handleSubmit(e){
+        e.preventDefault();
+        setCity("")
+        
+        if(city){
+            try{
+                const weatherData = await getWeatherData(city);
+                displayWeatherData(weatherData);
+            }
+            catch(error){
+                console.error(error)
+                    
+            }
+        };
+    };
+    
+    function displayWeatherData(data){
+ 
+        const {name: city,
+               main: {humidity, temp}, 
+               weather:[{id, description}] } = data;
+        console.log(city + "," + humidity +  "," + id);
+
+        setWeather({city, humidity, temp, id, description})
+    };
+
+    return(
+        <div className="container">
+            < Recent/>
+            <div className="forecast-container">
+            <form onSubmit={handleSubmit} className="seach-bar">
+                <input type="text" className="" placeholder="Enter city name..." onChange={(e)=> setCity(e.target.value)}  value={city}/>
+                <button type="submit">Search</button>
+            </form>
             <div className="display-result">
                 <h3>countries</h3>
                 <div className="wheather-info">
-                    <h4>humidity</h4>
-                    <h4>wind</h4>
-                    <h4>condition</h4>
+                    {weather &&(
+                    <>
+                        <h4>city:{weather.city}</h4>
+                        <h4>humidity:{weather.humidity}%</h4>
+                        <h4>Temp: {weather.temp}</h4>
+                        <h4>disc: {weather.description}</h4> 
+                    </>
+                    )}
                 </div>
-
+            </div>
             </div>
         </div>
-    </div>
+        
   );
 
 }
